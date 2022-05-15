6cb01a14abeca45c4a434d504dd8e51993cb6165
yogesh- <yogesh.gupta38@gmail.com>
2022-05-15 21:41:38 +0530

    updated video db,maade video card

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -3,18 +3,126 @@
  * You can add videos of your wish with different attributes
  * */
 
+import uuid from "react-uuid";
+
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: uuid(),
+    title: "Next.js Crash Course",
+    creator: "Traversy Media",
+    views: "592k",
+    subscribers: "1.85M",
+    duration: "1:09:44",
+    category: "trending",
+    videoId: "mTz0GXj8NN0",
+    thumbnail: "https://i3.ytimg.com/vi/mTz0GXj8NN0/maxresdefault.jpg",
+    creator_icon:
+      "https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s900-c-k-c0x00ffffff-no-rj",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: uuid(),
+    title: "Should You Use Tailwind CSS?",
+    creator: "Web Dev Simplified",
+    views: "182k",
+    subscribers: "927k",
+    duration: "07:46",
+    category: "trending",
+    videoId: "hdGsFpZ0J2E",
+    thumbnail: "https://img.youtube.com/vi/hdGsFpZ0J2E/maxresdefault.jpg",
+    creator_icon:
+      "https://yt3.ggpht.com/ytc/AKedOLQpvSjzSCSo8ZKCjBZS7TRX7omb_kyQirh2zgEY=s900-c-k-c0x00ffffff-no-rj",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    _id: uuid(),
+    title: "I built a chat app in 7 minutes with React & Firebase",
+    creator: "Fireship",
+    views: "784k",
+    subscribers: "1.32M",
+    duration: "08:52",
+    category: "trending",
+    videoId: "zQyrwxMPm88",
+    thumbnail: "https://img.youtube.com/vi/zQyrwxMPm88/maxresdefault.jpg",
+    creator_icon:
+      "	https://yt3.ggpht.com/ytc/AKedOLR5CDv14gL4DQ7I4gxIlBMY6u-CNsq2qfeev48R2g=s900-c-k-c0x00ffffff-no-rj",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    _id: uuid(),
+    title: "Learn flexbox the easy way",
+    creator: "Kevin Powell",
+    views: "154k",
+    subscribers: "528k",
+    duration: "34:03",
+    category: "trending",
+    videoId: "u044iM9xsWU",
+    thumbnail: "https://img.youtube.com/vi/u044iM9xsWU/maxresdefault.jpg",
+    creator_icon:
+      "https://yt3.ggpht.com/ytc/AKedOLT1tLx7BgAa7_SHaXE0hDRjQu1BRihsEDZ3lmUUcg=s900-c-k-c0x00ffffff-no-rj",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    _id: uuid(),
+    title: "The Story of Concurrent React",
+    creator: "uidotdev",
+    views: "107k",
+    subscribers: "52k",
+    duration: "11:58",
+    category: "trending",
+    videoId: "NZoRlVi3MjQ",
+    thumbnail: "https://img.youtube.com/vi/NZoRlVi3MjQ/maxresdefault.jpg",
+    creator_icon:
+      "https://yt3.ggpht.com/4-_80xHFXwX27JazINVIscTnJMZy…MrPJfB1wTxvPfRmFFJfaQ8=s900-c-k-c0x00ffffff-no-rj",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    _id: uuid(),
+    title: "Git Tutorial for Beginners",
+    creator: "Programming with Mosh",
+    views: "1M",
+    subscribers: "2.48M",
+    duration: "1:09:13",
+    category: "trending",
+    videoId: "8JJ101D3knE",
+    thumbnail: "https://img.youtube.com/vi/8JJ101D3knE/maxresdefault.jpg",
+    creator_icon:
+      "https://yt3.ggpht.com/tBEPr-zTNXEeae7VZKSZYfiy6azz…2PXSwzMBKVR7W0MI7gyND8=s900-c-k-c0x00ffffff-no-rj",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    _id: uuid(),
+    title: "What is an API?",
+    creator: "CodeWithHarry",
+    views: "900k",
+    subscribers: "2.7M",
+    duration: "12:35",
+    category: "trending",
+    videoId: "E0Qqpn8ymko",
+    thumbnail: "https://img.youtube.com/vi/E0Qqpn8ymko/maxresdefault.jpg",
+    creator_icon:
+      "https://yt3.ggpht.com/ytc/AKedOLT3EnMXtIOvDT4CL7obl0-acSZCBhMuapXBQFksVQ=s900-c-k-c0x00ffffff-no-rj",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    _id: uuid(),
+    title: "MUI5 Crash Course",
+    creator: "Laith Academy",
+    views: "57k",
+    subscribers: "29k",
+    duration: "1:38:34",
+    category: "trending",
+    videoId: "o1chMISeTC0",
+    thumbnail: "https://img.youtube.com/vi/o1chMISeTC0/maxresdefault.jpg",
+    creator_icon:
+      "https://yt3.ggpht.com/t_TrBp9Vyn2nhEJh5uz6Z9f8JbxV…nTL1y8dCqP7VWU4lLLmBmw=s900-c-k-c0x00ffffff-no-rj",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
   },
